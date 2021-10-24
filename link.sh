4c7e0a9a72533956bcf8d9b74ef591187f4d46d5
Haydon Ryan <haydon.ryan@gmail.com>
2021-10-24 14:53:00 +0000

    updating mode and fixing a typo

--- link.sh
+++ link.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
 
 cd ~
 ln -s .haydons-config/.vimrc .vimrc
 ln -s .haydons-config/.tmux.conf .tmux.conf
-ls -s .haydons-config/.Xmodmap .Xmodmap
+ln -s .haydons-config/.Xmodmap .Xmodmap
