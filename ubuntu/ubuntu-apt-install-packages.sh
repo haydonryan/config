7458cef53c06235475a7b255fcc3a0de0a7714c3
Haydon Ryan <haydon.ryan@gmail.com>
2021-10-30 09:04:52 -0500

    added packages to ubuntu base install

--- ubuntu/ubuntu-apt-install-packages.sh
+++ ubuntu/ubuntu-apt-install-packages.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-apt install git alpine-sdk curl vim go tmux ssh openssh nmap google-test cmake apt install golang
+apt install -y git alpine-sdk curl vim go tmux ssh openssh nmap google-test cmake golang gnome-tweaks
+apt install -y gimp inkscape krita
+
+# Shotcut is a free video editor
