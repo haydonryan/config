8d3d5e001a0d3f6fc530e544c12cec085f483436
Haydon Ryan <haydon.ryan@gmail.com>
2021-10-30 10:12:36 -0500

    adding golang-golang-x-tools to ubuntu

--- ubuntu/ubuntu-apt-install-packages.sh
+++ ubuntu/ubuntu-apt-install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install -y git alpine-sdk curl vim go tmux ssh openssh nmap google-test cmake golang gnome-tweaks
+apt install -y git alpine-sdk curl vim go tmux ssh openssh nmap google-test cmake golang golang-golang-x-tools gnome-tweaks
 apt install -y gimp inkscape krita
 
 # Shotcut is a free video editor
