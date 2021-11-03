ba33aea81548d930254883df3b55418558f8fb28
Haydon Ryan <haydon.ryan@gmail.com>
2021-11-03 14:22:37 -0500

    making script check if update already exists in crontab

--- ubuntu/add-update-to-chron.sh
+++ ubuntu/add-update-to-chron.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cp update.sh /usr/local/bin/
-echo "01 * * * * root /usr/local/bin/update.sh >> /home/haydon/update.log" >> /etc/crontab
-
+grep update.sh /etc/crontab>/dev/null || echo "01 * * * * root /usr/local/bin/update.sh >> /home/haydon/update.log" >> /etc/crontab
+cat /etc/crontab
