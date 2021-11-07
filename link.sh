65e825427b4921d7b56fc3ca8dd98613cbaacac8
Haydon Ryan <haydon.ryan@gmail.com>
2021-11-07 11:44:24 -0600

    adding bashrc

--- link.sh
+++ link.sh
@@ -5,3 +5,4 @@ ln -s .haydons-config/.tmux.conf .tmux.conf
 ln -s .haydons-config/.Xmodmap .Xmodmap
 ln .haydons-config/.bash_aliases ./.bash_aliases
 ln -s .haydons-config/.gitconfig .gitconfig
+ln -s .haydons-config/.bashrc .bashrc
