cce5934af302f240f63304bda0bd8fe6aa0a8605
Haydon Ryan <haydon.ryan@gmail.com>
2021-11-07 13:10:22 -0600

    change link.sh to be relative to home

--- link.sh
+++ link.sh
@@ -1,8 +1,8 @@
+#!/bin/bash
 
-cd ~
-ln -s .haydons-config/.vimrc .vimrc
-ln -s .haydons-config/.tmux.conf .tmux.conf
-ln -s .haydons-config/.Xmodmap .Xmodmap
-ln -s .haydons-config/.bash_aliases ./.bash_aliases
-ln -s .haydons-config/.gitconfig .gitconfig
-ln -s .haydons-config/.bashrc .bashrc
+ln -s ~/.haydons-config/.vimrc ~/.vimrc
+ln -s ~/.haydons-config/.tmux.conf ~/.tmux.conf
+ln -s ~/.haydons-config/.Xmodmap ~/.Xmodmap
+ln -s ~/.haydons-config/.bash_aliases ~/.bash_aliases
+ln -s ~/.haydons-config/.gitconfig ~/.gitconfig
+ln -s ~/.haydons-config/.bashrc ~/.bashrc
