e9e7c5b61b372ed0970f613c21dabccc6843109a
Haydon Ryan <haydon.ryan@gmail.com>
2021-11-07 08:25:37 -0600

    adding .gitconfig and extra steps to git script

--- setup-git.sh
+++ setup-git.sh
@@ -2,4 +2,6 @@
 
 git config --global user.email "haydon.ryan@gmail.com"
 git config --global user.name "Haydon Ryan"
-git config pull.rebase true
+git config --global pull.rebase true
+git config --global fetch.prune true
+git config --global diff.colorMoved zebra
