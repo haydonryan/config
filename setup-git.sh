3de3b4fb5ea7219f4a3b805536c568d16918b62f
Haydon Ryan <haydon.ryan@gmail.com>
2021-11-07 08:32:23 -0600

    removing git setup in favor of .gitconfig

--- setup-git.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-git config --global user.email "haydon.ryan@gmail.com"
-git config --global user.name "Haydon Ryan"
-git config --global pull.rebase true
-git config --global fetch.prune true
-git config --global diff.colorMoved zebra
