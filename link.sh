eec7e3259689e87f2e745ccae7f8b06b299e8599
Haydon Ryan <haydon.ryan@gmail.com>
2021-11-13 19:06:41 +0000

    adding some helpful text to the screen for link

--- link.sh
+++ link.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
-
+echo "Linking config files from .haydons-config"
+echo "It will not overwrite any existing files"
+echo "========================================"
+echo ""
+echo "linking .vimrc"
 ln -s ~/.haydons-config/.vimrc ~/.vimrc
+echo "Linking .tmuz.conf"
 ln -s ~/.haydons-config/.tmux.conf ~/.tmux.conf
+echo "linking .Xmodmap"
 ln -s ~/.haydons-config/.Xmodmap ~/.Xmodmap
+echo "linking .bash_alias"
 ln -s ~/.haydons-config/.bash_aliases ~/.bash_aliases
+echo "linking .gitconfig"
 ln -s ~/.haydons-config/.gitconfig ~/.gitconfig
+echo "linking .bashrc"
 ln -s ~/.haydons-config/.bashrc ~/.bashrc
