eedcaafc05e12f4c974e504c0464e183c2c8ea51
Haydon Ryan <haydon.ryan@gmail.com>
2022-01-27 11:31:49 -0600

    adding nodejs for github copilot

--- ubuntu/ubuntu-apt-install-packages.sh
+++ ubuntu/ubuntu-apt-install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt install -y git alpine-sdk curl vim go tmux ssh openssh nmap google-test cmake golang golang-golang-x-tools gnome-tweaks vlc
-apt install -y gimp inkscape krita gddrescue
+apt install -y gimp inkscape krita gddrescue nodejs
 
 # Shotcut is a free video editor
