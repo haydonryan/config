fb0151a463699b953cafece686819599a39aab6f
Haydon Ryan <haydon.ryan@gmail.com>
2022-01-27 11:43:29 -0600

    adding neovim repository as ubuntu isn't uptodate

--- ubuntu/ubuntu-apt-install-packages.sh
+++ ubuntu/ubuntu-apt-install-packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+sudo add-apt-repository ppa:neovim-ppa/stable # add neovim repo
 apt install -y git alpine-sdk curl vim go tmux ssh openssh nmap google-test cmake golang golang-golang-x-tools gnome-tweaks vlc
-apt install -y gimp inkscape krita gddrescue nodejs
+apt install -y gimp inkscape krita gddrescue nodejs neovim
 
 # Shotcut is a free video editor
