85e70286487e00a99e5d442c424219d863497d22
Haydon Ryan <haydon.ryan@gmail.com>
2022-10-09 12:15:18 -0500

    Removing links as migrating to dotfiles.

--- link.sh
+++ link.sh
@@ -3,8 +3,8 @@ echo "Linking config files from .haydons-config"
 echo "It will not overwrite any existing files"
 echo "========================================"
 echo ""
-echo "linking .vimrc"
-ln -s ~/.haydons-config/.vimrc ~/.vimrc
+#echo "linking .vimrc"
+#ln -s ~/.haydons-config/.vimrc ~/.vimrc
 echo "Linking .tmuz.conf"
 ln -s ~/.haydons-config/.tmux.conf ~/.tmux.conf
 echo "linking .Xmodmap"
@@ -13,8 +13,8 @@ echo "linking .bash_alias"
 ln -s ~/.haydons-config/.bash_aliases ~/.bash_aliases
 echo "linking .gitconfig"
 ln -s ~/.haydons-config/.gitconfig ~/.gitconfig
-echo "linking .bashrc"
-ln -s ~/.haydons-config/.bashrc ~/.bashrc
+#echo "linking .bashrc"
+#ln -s ~/.haydons-config/.bashrc ~/.bashrc
 
 if [ $SHELL == "/bin/ash" ]; then
   echo "linking ash profile script"
