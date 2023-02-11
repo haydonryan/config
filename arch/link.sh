172edc7dcef4c2869459f6e6225e0017c66dec4b
Haydon Ryan <haydon.ryan@gmail.com>
2023-02-11 11:32:24 -0600

    removing arch nvim and moving notes to main page

--- arch/link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Linking config files from .haydons-config"
-echo "It will not overwrite any existing files"
-echo "========================================"
-echo ""
-echo "linking .vimrc"
-ln -s ~/.haydons-config/arch/.config/nvim ~/.config/nvim
-
